00bf5a7a9c531c6a6fc8908baf3f281b75638b04
Danny Rerucha <DannyRerucha@gmail.com>
2014-11-16 21:41:33 -0500

    Firework proof of concept.

---
+++ FireworkFestival/Assets/danny/scripts/Launcher.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour
+{
+	public GameObject[] particles;
+
+	private GameObject current_go;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+
+	public void Launch()
+	{
+		// DEBUG.
+//		Debug.Log( "Inside Launch()." );
+
+		int i = Random.Range( 0, particles.Length );
+
+		GameObject particle_go = particles[i];
+		if ( particle_go.GetComponent( typeof( ParticleSystem ) ) ) {
+//			ParticleSystem ps = ( ParticleSystem )particle_go.GetComponent( typeof( ParticleSystem ) );
+//			ps.Emit( 1 );
+
+			if ( current_go ) {
+				Destroy( current_go );
+			}
+
+			GameObject go = ( GameObject )Instantiate( particle_go, this.transform.position, this.transform.rotation );
+			current_go = go;
+
+			ParticleSystem ps = ( ParticleSystem )current_go.GetComponent( typeof( ParticleSystem ) );
+//			ps.Play();
+			ps.Emit( 1 );
+
+			// DEBUG.
+//			Debug.Log( "Trying to call ParticleSystem methods." );
+		}
+	}
+}
