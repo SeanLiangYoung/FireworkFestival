f4eb177dedf26e452504f695bbfad88ac497e1c4
Danny Rerucha <DannyRerucha@gmail.com>
2014-11-18 20:34:06 -0500

    Add alpha particle systems functionality.

---
+++ FireworkFestival/Assets/danny/scripts/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{	
+	public LauncherManager launcher_manager;
+	
+	void Update()
+	{
+		int num_shells = 1;
+
+		if ( Input.GetKeyDown( KeyCode.A ) ) {
+			launcher_manager.LaunchFireworks( 'A', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.S ) ) {
+			launcher_manager.LaunchFireworks( 'S', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.D ) ) {
+			launcher_manager.LaunchFireworks( 'D', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.F ) ) {
+			launcher_manager.LaunchFireworks( 'F', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.J ) ) {
+			launcher_manager.LaunchFireworks( 'J', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.K ) ) {
+			launcher_manager.LaunchFireworks( 'K', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.L ) ) {
+			launcher_manager.LaunchFireworks( 'L', num_shells );
+		}
+		if ( Input.GetKeyDown( KeyCode.Semicolon ) ) {
+			launcher_manager.LaunchFireworks( ';', num_shells );
+		}
+	}
+}
--- FireworkFestival/Assets/danny/scripts/LauncherManager.cs
+++ FireworkFestival/Assets/danny/scripts/LauncherManager.cs
@@ -5,48 +5,44 @@ public class LauncherManager : MonoBehaviour
 {
 	public GameObject[] launchers;
 
-	// DEBUG.
-	private float timeBetweenLaunches;
-	private float timeSinceLastLaunch;
-
-	// Use this for initialization
-	void Start()
+	public void LaunchFireworks( char c, int num_shells )
 	{
-		// DEBUG.
-		timeBetweenLaunches = 2.0f;
-		timeSinceLastLaunch = 0.0f;
+		int launcher_index = 0;
+
+		if ( c == 'A' ) {
+			launcher_index = 0;
+		}
+		else if ( c == 'S' ) {
+			launcher_index = 1;
+		}
+		else if ( c == 'D' ) {
+			launcher_index = 2;
+		}
+		else if ( c == 'F' ) {
+			launcher_index = 3;
+		}
+		else if ( c == 'J' ) {
+			launcher_index = 4;
+		}
+		else if ( c == 'K' ) {
+			launcher_index = 5;
+		}
+		else if ( c == 'L' ) {
+			launcher_index = 6;
+		}
+		else if ( c == ';' ) {
+			launcher_index = 7;
+		}
+
+		LaunchFireworks( launcher_index, num_shells );
 	}
 	
-	// Update is called once per frame
-	void Update()
+	private void LaunchFireworks( int launcher_index, int num_shells )
 	{
-		// DEBUG.
-		timeSinceLastLaunch += Time.deltaTime;
-		if ( timeSinceLastLaunch > timeBetweenLaunches ) {
-			timeSinceLastLaunch = 0.0f;
-			LaunchFireworks( 1 );
-		}
-	}
-
-	// Launch n number of fireworks randomly from the Launcher game objects in the scene.
-	public void LaunchFireworks( int n )
-	{
-		// DEBUG.
-//		Debug.Log( "Inside LaunchFireworks()." );
-
-		int start_index = Random.Range( 0, launchers.Length );
-
-		for ( int i = 0; i < n; ++i ) {
-			int launcher_index = start_index + i;
-			while ( launcher_index >= launchers.Length ) {
-				launcher_index %= launchers.Length;
-			}
-
-			GameObject launcher_go = launchers[launcher_index];
-			if ( launcher_go.GetComponent( typeof( Launcher ) ) ) {
-				Launcher launcher_script = ( Launcher )launcher_go.GetComponent( typeof( Launcher ) );
-				launcher_script.Launch();
-			}
+		GameObject launcher_go = launchers[launcher_index];
+		if ( launcher_go.GetComponent( typeof( Launcher ) ) ) {
+			Launcher launcher_script = ( Launcher )launcher_go.GetComponent( typeof( Launcher ) );
+			launcher_script.Launch( num_shells );
 		}
 	}
 }
