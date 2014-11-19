c38dea8bdff72786d5206f5dccb7bb74ed7661d7
Max Gilbert <gilbertlmax@gmail.com>
2014-11-18 19:09:35 -0500

    public beat duration list

--- FireworkFestival/Assets/MaxsFolder/BeatDetection/BeatCreator.cs
+++ FireworkFestival/Assets/MaxsFolder/BeatDetection/BeatCreator.cs
@@ -8,6 +8,7 @@ public class BeatCreator : MonoBehaviour {
 
 	public string fileName;
 	private List<float[]> _beatList;
+	public List<float> beatDurations;
 	public List<float> beats;
 	public AudioSource song;
 	public GameObject cube;
@@ -20,8 +21,19 @@ public class BeatCreator : MonoBehaviour {
 	private float _timeOffset;
 	private int _currentBeat;
 
-
-
+	public static BeatCreator instance;
+	
+	
+	public static BeatCreator Instance
+	{
+		get 
+		{
+			return instance;
+		}
+	}
+	void Awake () {
+		instance = this;
+	}
 	// Use this for initialization
 	void Start () {
 		_currentBeat = 0;
@@ -42,13 +54,15 @@ public class BeatCreator : MonoBehaviour {
 			Debug.LogError(e.Message);
 		}
 		beats = new List<float>();
+		beatDurations = new List<float>();
 		float currTime = 0.0f;
 		for (int i = 0; i < _beatList.Count; i++) {
 			float e = _beatList[i][1];
 			float E = _beatList[i][2];
 			if (e > (constant * E)) {
 				beats.Add(_beatList[i][0]);// - currTime);
-				//currTime = _beatList[i][0];
+				beatDurations.Add(_beatList[i][0]-currTime);
+				currTime = _beatList[i][0];
 			}
 		}
 		PlaySong();
