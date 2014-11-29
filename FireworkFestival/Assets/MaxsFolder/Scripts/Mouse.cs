5638cd4bf7abedba064604efaafe4a79d0c3fa15
Max Gilbert <gilbertlmax@gmail.com>
2014-11-28 21:35:48 -0500

    firework mouse effect

---
+++ FireworkFestival/Assets/MaxsFolder/Scripts/Mouse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+	public float fireworkTime;
+	public int minFireworks;
+	public int maxFireworks;
+	private MouseLauncher _launcher;
+
+	private float _elapsedTime = 0.0f;
+
+	//public GameObject fireworksPlane;
+	// Use this for initialization
+	void Start () {
+		_launcher = gameObject.GetComponent<MouseLauncher>() as MouseLauncher;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 launchPosition = new Vector3();//Input.mousePosition;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit,300.0f)) {
+			launchPosition = hit.point;
+			//launchPosition.z-=2.0f;
+		}
+		//launchPosition.z = 150;
+		
+		_elapsedTime += Time.deltaTime;
+		if (_elapsedTime >= fireworkTime )
+		{
+			int numFireworks = Random.Range(minFireworks,maxFireworks);
+			//Debug.LogError(launchPosition.ToString());
+			_launcher.LaunchFrom(numFireworks,launchPosition);
+			_elapsedTime = 0.0f;
+		}
+	}
+}
---
+++ FireworkFestival/Assets/MaxsFolder/Scripts/MouseLauncher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLauncher : MonoBehaviour
+{
+	public GameObject[] particles;
+	public int particle_index;
+
+	private GameObject current_go;
+
+	public void LaunchFrom( int num_shells, Vector3 launchPosition )
+	{
+		GameObject particle_go = particles[particle_index];
+		if ( particle_go.GetComponent( typeof( ParticleSystem ) ) ) {
+//			if ( current_go ) {
+//				Destroy( current_go );
+//			}
+
+			if ( !current_go ) {
+				GameObject go = ( GameObject )Instantiate( particle_go, this.transform.position, this.transform.rotation );
+				current_go = go;
+			}
+			current_go.gameObject.transform.position = launchPosition;
+			ParticleSystem ps = ( ParticleSystem )current_go.GetComponent( typeof( ParticleSystem ) );
+			ps.Emit( num_shells );
+		}
+	}
+}
