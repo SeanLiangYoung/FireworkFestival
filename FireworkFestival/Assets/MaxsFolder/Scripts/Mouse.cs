19fc559766d991f097c762a7747dc2586177d4ef
Max Gilbert <gilbertlmax@gmail.com>
2014-12-03 02:07:22 -0500

    water

--- FireworkFestival/Assets/MaxsFolder/Scripts/Mouse.cs
+++ FireworkFestival/Assets/MaxsFolder/Scripts/Mouse.cs
@@ -20,14 +20,16 @@ public class Mouse : MonoBehaviour {
 		Vector3 launchPosition = new Vector3();//Input.mousePosition;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit,300.0f)) {
+		bool launch = false;
+		if (Physics.Raycast(ray, out hit,1000.0f) && hit.collider.tag.Equals("RaycastPlane")) {
 			launchPosition = hit.point;
+			launch = true;
 			//launchPosition.z-=2.0f;
 		}
 		//launchPosition.z = 150;
 		
 		_elapsedTime += Time.deltaTime;
-		if (_elapsedTime >= fireworkTime )
+		if (launch && _elapsedTime >= fireworkTime )
 		{
 			int numFireworks = Random.Range(minFireworks,maxFireworks);
 			//Debug.LogError(launchPosition.ToString());
